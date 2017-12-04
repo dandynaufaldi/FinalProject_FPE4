cb36215b5ece0d1ebb5a90bb0fc8b33168e070c6
nawamkihafahd <nawamkihafahd@gmail.com>
2017-12-05 00:50:10 +0700

    tail -> basic -n and mode tempalate

--- tail.c
+++ tail.c
@@ -4,7 +4,16 @@
 #include "fcntl.h"
 #include "fs.h"
 
-
+int strint(char* a)
+{
+	int result=0;
+	int i=0;
+	for(i=0; a[i] != '\0'; i++)
+	{
+		result = result*10 + a[i] - '0';
+	}
+	return result;
+}
 
 void tailline(char* path, int linestoprint)
 {
@@ -48,19 +57,66 @@ void tailline(char* path, int linestoprint)
 
 int main(int argc, char* argv[])
 {
+	int a;
 	int fd = -1;
 	if(argc <=1)
 	printf(1, "Usage: tail <mode> <filename>");
-	else if(argc == 2)
+	else if(argc > 1)
 	{
-		fd = open(argv[1], 0);
-		if(fd < 0)
-		printf(1, "Error: cannot open %s\n", argv[1]);
+		if(argv[1][0] == '-')
+		{
+			if(argv[1][1] == 'n')
+			{
+				if(argc < 4)
+				{
+					printf(1, "Error: Usage tail -n <NUM> <filename>\n");
+				}
+				else
+				{
+					int i;
+					a=strint(argv[2]);
+					for(i=3;i<argc;i++)
+					{
+						fd = open(argv[i], 0);
+						if(fd < 0)
+						printf(1, "Error: cannot open %s\n", argv[i]);
+						else
+						{
+							close(fd);
+							tailline(argv[i], a);
+							printf(1, "\n");
+						}
+					}
+				}
+			}
+			else if(argv[1][1] == 'c')
+			{
+			}
+			else if(argv[1][1] == 'q')
+			{
+			}
+			else if(argv[1][1] == 'v')
+			{
+			}
+		}
 		else
 		{
-			close(fd);
-			tailline(argv[1], 10);
+			int i;
+			for(i=1;i<argc;i++)
+			{
+				fd = open(argv[i], 0);
+				if(fd < 0)
+				printf(1, "Error: cannot open %s\n", argv[i]);
+				else
+				{
+					close(fd);
+					tailline(argv[i], 10);
+					printf(1, "\n");
+				}
+			}
+			
 		}
+		
 	}
 	return 0;
 }
