03791391dcd0e07495e10f5b0058f8e37f31e742
nawamkihafahd <nawamkihafahd@gmail.com>
2017-12-05 21:02:50 +0700

    tail -> mode -c, single option

--- README.md
+++ README.md
@@ -23,13 +23,13 @@ Command pilihan
 - [ ] tree -d (hanya tampilkan direktori)
 - [ ] tree -o (simpan ke output file)
 - [ ] tree -p (tampilkan hak akses file, cancel aja ya :( )
-- [ ] tail
-- [ ] tail -c NUM (tampilkan NUM bytes terakhir)
-- [ ] tail -c +NUM (tampilkan mulai dari bytes ke-NUM)
-- [ ] tail -n NUM (tampilkan NUM lines terakhir)
-- [ ] tail -n +NUM (tampilkan mulai dari line ke-NUM)
-- [ ] tail -v tampilkan header nama file
-- [ ] tail -q tidak menampilkan header nama file
+- [x] tail
+- [x] tail -c NUM (tampilkan NUM bytes terakhir)
+- [x] tail -c +NUM (tampilkan mulai dari bytes ke-NUM)
+- [x] tail -n NUM (tampilkan NUM lines terakhir)
+- [x] tail -n +NUM (tampilkan mulai dari line ke-NUM)
+- [x] tail -v tampilkan header nama file
+- [x] tail -q tidak menampilkan header nama file
 - [ ] sort
 - [ ] sort -r (sort descending / besar-kecil)
 - [ ] sort -o (simpan ke file output)
--- tail.c
+++ tail.c
@@ -15,12 +15,51 @@ int strint(char* a)
 	return result;
 }
 
-void tailline(char* path, int linestoprint)
+void tailbyte(char* path, int bytestoprint, int pflag)
 {
 	int fd;
 	char line[1];
 	int status;
-	int totline=1;
+	int size=0;
+	fd = open(path, 0);
+	while((status = read(fd, line, sizeof(line))) > 0)
+	{
+		size++;
+	}
+	if(status<0)
+	{
+		printf(1, "Error read file\n");
+	}
+	int skip = size - bytestoprint;
+	if(skip < 0)
+	{
+		skip = 0;
+		if(pflag)
+		{
+			printf(1, "==> %s <==\n", path);
+		}
+	}
+	close(fd);
+	fd = open(path, 0);
+	while(skip)
+	{
+		status = read(fd, line, sizeof(line));
+		skip--;
+		
+	}
+	while((status = read(fd, line, sizeof(line))) > 0)
+	{
+		printf(1, "%c", line[0]);
+	}
+	close(fd);
+}
+
+void tailline(char* path, int linestoprint, int pflag)
+{
+	int fd;
+	char line[1];
+	int status;
+	int totline=0;
 	fd = open(path, 0);
 	while((status = read(fd, line, sizeof(line))) > 0)
 	{
@@ -35,6 +74,10 @@ void tailline(char* path, int linestoprint)
 	if(skip < 0)
 	{
 		skip = 0;
+		if(pflag)
+		{
+			printf(1, "==> %s <==\n", path);
+		}
 	}
 	close(fd);
 	fd = open(path, 0);
@@ -83,14 +126,33 @@ int main(int argc, char* argv[])
 						else
 						{
 							close(fd);
-							tailline(argv[i], a);
-							printf(1, "\n");
+							tailline(argv[i], a, 1);
 						}
 					}
 				}
 			}
 			else if(argv[1][1] == 'c')
 			{
+				if(argc < 4)
+				{
+					printf(1, "Error: Usage tail -c <NUM> <filename>\n");
+				}
+				else
+				{
+					int i;
+					a=strint(argv[2]);
+					for(i=3;i<argc;i++)
+					{
+						fd = open(argv[i], 0);
+						if(fd < 0)
+						printf(1, "Error: cannot open %s\n", argv[i]);
+						else
+						{
+							close(fd);
+							tailbyte(argv[i], a, 1);
+						}
+					}
+				}
 			}
 			else if(argv[1][1] == 'q')
 			{
@@ -103,8 +165,7 @@ int main(int argc, char* argv[])
 					else
 					{
 						close(fd);
-						tailline(argv[i], 10);
-						printf(1, "\n");
+						tailline(argv[i], 10, 0);
 					}
 				}
 			}
@@ -120,8 +181,7 @@ int main(int argc, char* argv[])
 					{
 						close(fd);
 						printf(1, "==> %s <==\n", argv[i]);
-						tailline(argv[i], 10);
-						printf(1, "\n");
+						tailline(argv[i], 10, 0);
 					}
 				}
 				
@@ -138,9 +198,7 @@ int main(int argc, char* argv[])
 				else
 				{
 					close(fd);
-					printf(1, "==> %s <==\n", argv[i]);
-					tailline(argv[i], 10);
-					printf(1, "\n");
+					tailline(argv[i], 10, 1);
 				}
 			}
 			
